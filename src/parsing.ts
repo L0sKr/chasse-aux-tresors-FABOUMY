467f51226dcfe431aa92f8c8f9ad085fc4574f8b
cfaboumyIppon <114093660+cfaboumyIppon@users.noreply.github.com>
2024-09-30 14:13:16 +0200

    feat: add mountains and treasures to map

--- src/parsing.ts
+++ src/parsing.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export function parseFileFromPath(filePath: string): void {
   const filePathInDirectory: string = path.join(process.cwd(), filePath);
-  let treasureMap: string[][];
+  let treasureMap: string[][] = [[]];
 
   lineReader.eachLine(filePathInDirectory, (line: string) => {
     line = line.replaceAll(" - ", "");
@@ -13,27 +13,40 @@ export function parseFileFromPath(filePath: string): void {
     switch (infoType) {
       case "C":
         treasureMap = createMap(+line[2], +line[3]);
-        console.log(treasureMap);
         break;
       case "M":
-        console.log("Mountain info : lat - " + line[2] + ",  lon - " + line[3]);
+        addMountain(treasureMap, +line[2], +line[3]);
         break;
       case "T":
-        console.log(
-          "Treasure info : lat - " +
-            line[2] +
-            ",  lon - " +
-            line[3] +
-            ", nb - " +
-            line[4]
-        );
+        addTreasures(treasureMap, +line[2], +line[3], +line[4]);
         break;
     }
+    console.log(treasureMap);
   });
+  // console.log(treasureMap);
 }
 
 function createMap(mapWidth: number, mapHeight: number): string[][] {
   return Array.from({ length: mapHeight }, () =>
     Array.from({ length: mapWidth }, () => ".")
   );
+}
+
+function addMountain(
+  map: string[][],
+  longitude: number,
+  latitude: number
+): string[][] {
+  map[longitude][latitude] = "M";
+  return map;
+}
+
+function addTreasures(
+  map: string[][],
+  longitude: number,
+  latitude: number,
+  nbTreasures: number
+): string[][] {
+  map[latitude][longitude] = `T(${nbTreasures})`;
+  return map;
 }
