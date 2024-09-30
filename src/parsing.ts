b168f7ed3cbdb980c1325d58b64113c5c3cf4e40
cfaboumyIppon <114093660+cfaboumyIppon@users.noreply.github.com>
2024-09-30 13:56:28 +0200

    feat: read each line and create map

--- src/parsing.ts
+++ src/parsing.ts
@@ -1,13 +1,39 @@
-import * as fs from "fs";
+// import * as fs from "fs";
+import * as lineReader from "line-reader";
 import path from "path";
 
 export function parseFileFromPath(filePath: string): void {
   const filePathInDirectory: string = path.join(process.cwd(), filePath);
-  fs.readFile(filePathInDirectory, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(data); // parsed file contents as a string
+  let treasureMap: string[][];
+
+  lineReader.eachLine(filePathInDirectory, (line: string) => {
+    line = line.replaceAll(" - ", "");
+
+    const infoType = line[0];
+    switch (infoType) {
+      case "C":
+        treasureMap = createMap(+line[2], +line[3]);
+        console.log(treasureMap);
+        break;
+      case "M":
+        console.log("Mountain info : lat - " + line[2] + ",  lon - " + line[3]);
+        break;
+      case "T":
+        console.log(
+          "Treasure info : lat - " +
+            line[2] +
+            ",  lon - " +
+            line[3] +
+            ", nb - " +
+            line[4]
+        );
+        break;
     }
   });
+}
+
+function createMap(mapWidth: number, mapHeight: number): string[][] {
+  return Array.from({ length: mapHeight }, () =>
+    Array.from({ length: mapWidth }, () => ".")
+  );
 }
