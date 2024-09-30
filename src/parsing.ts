f00d30f2eb3c6b5dadd3ed79d55d67da812e30be
cfaboumyIppon <114093660+cfaboumyIppon@users.noreply.github.com>
2024-09-30 18:34:52 +0200

    feat: use synchronous function to read entry file

--- src/parsing.ts
+++ src/parsing.ts
@@ -1,58 +1,55 @@
 // import * as fs from "fs";
-import * as lineReader from "line-reader";
-import path from "path";
+import fs from 'fs';
+import path from 'path';
+
+type AdvInfos = { [key: string]: AdvInfo };
+type AdvInfo = { orientation: string; moves: string };
 
 export function parseFileFromPath(filePath: string): void {
   const filePathInDirectory: string = path.join(process.cwd(), filePath);
-  let treasureMap: string[][] = [[]];
+
+  const fileContent = fs.readFileSync(filePathInDirectory, 'utf-8');
+  const lines = fileContent.split('\n');
+
+  let treasureMap: string[][] = [];
   const advInfos: AdvInfos = {};
 
-  lineReader.eachLine(filePathInDirectory, (line: string) => {
-    const lineInfo: string[] = line.replaceAll(" ", "").split("-");
+  for (const line of lines) {
+    const lineInfo: string[] = line.replaceAll(' ', '').split('-');
     const infoType = line[0];
 
     switch (infoType) {
-      case "C":
+      case 'C':
         treasureMap = createMap(+lineInfo[1], +lineInfo[2]);
         break;
-      case "M":
-        treasureMap = addMountain(treasureMap, +lineInfo[1], +lineInfo[2]);
+      case 'M':
+        addMountain(treasureMap, +lineInfo[1], +lineInfo[2]);
         break;
-      case "T":
-        treasureMap = addTreasures(
-          treasureMap,
-          +lineInfo[1],
-          +lineInfo[2],
-          +lineInfo[3]
-        );
+      case 'T':
+        addTreasures(treasureMap, +lineInfo[1], +lineInfo[2], +lineInfo[3]);
         break;
-      case "A":
-        treasureMap = addAdventurer(
-          treasureMap,
-          lineInfo[1],
-          +lineInfo[2],
-          +lineInfo[3]
-        );
+      case 'A':
+        addAdventurer(treasureMap, lineInfo[1], +lineInfo[2], +lineInfo[3]);
         setAdventurerInfos(advInfos, lineInfo[1], lineInfo[4], lineInfo[5]);
         break;
     }
-    console.log(treasureMap);
-    console.log(advInfos);
-  });
+  }
+  console.log(treasureMap);
+  console.log(advInfos);
 }
 
 function createMap(mapWidth: number, mapHeight: number): string[][] {
   return Array.from({ length: mapHeight }, () =>
-    Array.from({ length: mapWidth }, () => ".")
+    Array.from({ length: mapWidth }, () => '.'),
   );
 }
 
 function addMountain(
   map: string[][],
   longitude: number,
-  latitude: number
+  latitude: number,
 ): string[][] {
-  map[latitude][longitude] = "M";
+  map[latitude][longitude] = 'M';
   return map;
 }
 
@@ -60,7 +57,7 @@ function addTreasures(
   map: string[][],
   longitude: number,
   latitude: number,
-  nbTreasures: number
+  nbTreasures: number,
 ): string[][] {
   map[latitude][longitude] = `T(${nbTreasures})`;
   return map;
@@ -70,20 +67,17 @@ function addAdventurer(
   map: string[][],
   name: string,
   longitude: number,
-  latitude: number
+  latitude: number,
 ): string[][] {
   map[latitude][longitude] = `A(${name})`;
   return map;
 }
 
-type AdvInfos = { [key: string]: AdvInfo };
-type AdvInfo = { orientation: string; moves: string };
-
 function setAdventurerInfos(
   infos: AdvInfos,
   name: string,
   orientation: string,
-  moves: string
+  moves: string,
 ): AdvInfos {
   infos[name] = { orientation, moves };
   return infos;
